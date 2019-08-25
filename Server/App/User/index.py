91b33ae49eb42c1ee831bf3211fb5a242621c6fb
parkjinhong03 <jinhong0719@naver.com>
2019-08-25 21:45:48 +0900

    Server & GUI 파일 분리 및 기본 틀 작성

---
+++ Server/App/User/index.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource
+# from App.User.Method.post import post
+
+class HandleUser(Resource):
+    def get(self):
+        return 'RESTful API'
+
+    def post(self):
+        return 'POST'
--- Server/run.py
+++ Server/run.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
+
+from App.User.index import HandleUser
+
+api.add_resource(HandleUser, '/user')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
