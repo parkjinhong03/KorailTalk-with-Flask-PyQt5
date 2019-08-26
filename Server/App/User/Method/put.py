dedfd103e6be869aad2975a310ec601505d1c963
parkjinhong03 <jinhong0719@naver.com>
2019-08-26 19:08:12 +0900

    JWT 토큰 인증을 이용한 PW 변경

--- Server/App/User/Method/put.py
+++ Server/App/User/Method/put.py
@@ -1,4 +1,7 @@
 from flask_restful import reqparse
+from flask_jwt_extended import get_jwt_identity
+import pymysql
+from DB.connect import db_connect
 
 
 def put():
@@ -7,6 +10,9 @@ def put():
     :parameter: pw, pw_check
     :return: status code
     200 - 성공
+    401 - JWT 존재 X or 만료됨
+    422 - JWT Decoding 오류
+    411 - 비밀번호 확인 불일치
     '''
 
     parser = reqparse.RequestParser()
@@ -14,6 +20,20 @@ def put():
     parser.add_argument('pw_check', type=str)
     args = parser.parse_args()
 
-    print(args['pw'], args['pw_check'])
+    _userPw = args['pw']
+    _userPwC = args['pw_check']
 
-    return 'ok'
+    if _userPw != _userPwC:
+        return {"message": "pw and pw_check do not match", "code": 411}, 411
+
+    current_user = get_jwt_identity()
+
+    db, cursor = db_connect()
+
+    sql = f'UPDATE userlog SET user_pw = "{_userPw}"  WHERE user_id = "{current_user}"'
+    cursor.execute(sql)
+
+    db.commit()
+    db.close()
+
+    return {"message": "Your password has changed normally.", "code": 200}, 200
