49b3e40f2cd4149b3d3eb40773d373d02398368f
parkjinhong03 <jinhong0719@naver.com>
2019-09-05 20:08:11 +0900

    출발역, 도착역, 날짜, 시간을 모두 고려한 기차 검색 API 구현

--- Server/App/Train/View/get.py
+++ Server/App/Train/View/get.py
@@ -1,6 +1,17 @@
 import pymysql
+import datetime
 from DB.connect import db, cursor
 from flask_restful import reqparse
+
+date_dict = {
+    "Monday": '1',
+    "Tuesday": '2',
+    "Wednesday": '3',
+    "Thursday": '4',
+    "Friday": '5',
+    "Saturday": '6',
+    "Sunday": '7'
+}
 
 
 def get():
@@ -20,16 +31,39 @@ def get():
     time = args['time']
     date = args['date']
 
-    sql = f'SELECT Train_Num, Train_Name, {start}, {end} FROM FromSeoulToBusan WHERE {start} != "NULL" AND {end} != "NULL"'
+    dt = datetime.date(int(date[0:4]), int(date[4:6]), int(date[6:8]))
+
+    sql = f'SELECT Train_Num, Train_Name, {start}, {end}, Remark FROM FromSeoulToBusan WHERE {start} != "NULL" AND {end} != "NULL"'
     cursor.execute(sql)
     train_inform = list(cursor.fetchall())
 
     for specific in train_inform:
+        if specific[4] != None and date_dict[dt.strftime("%A")] not in str(specific[4]):
+            continue
+        if (specific[2] // 100) * 60 + (specific[2] % 100) < (int(time)//100 * 60 + int(time) % 100):
+            continue
+
         specific_dict = {}
         specific_dict['train_num'] = specific[0]
         specific_dict['train_name'] = specific[1]
-        specific_dict['start_time'] = specific[2]
-        specific_dict['end_time'] = specific[3]
+
+        time_list = [str(specific[2]), str(specific[3])]
+        time_real_list = []
+
+        for i in time_list:
+            if len(i) != 4:
+                for _ in range(4 - len(i)):
+                    i = '0' + i
+                i = i[:2] + ":" + i[2:]
+                if i[:1] == '0':
+                    i = i[1:]
+            else:
+                i = i[:2] + ":" + i[2:]
+
+            time_real_list.append(i)
+
+        specific_dict['start_time'] = time_real_list[0]
+        specific_dict['end_time'] = time_real_list[1]
 
         return_dict[count] = specific_dict
         count += 1
