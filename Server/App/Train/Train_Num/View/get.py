9726eb19850526a9aa8a655fa11e62bf0b0823e6
parkjinhong03 <jinhong0719@naver.com>
2019-09-09 22:40:25 +0900

    예매 상태 조회 API까지 구현 완료

--- Server/App/Train/Train_Num/View/get.py
+++ Server/App/Train/Train_Num/View/get.py
@@ -1,6 +1,19 @@
 from flask_restful import reqparse
 from DB.connect import db, cursor
 
+train_to_num = {
+        'Seoul': 1,
+        'Gwangmyeong': 2,
+        'Cheonan_Asan': 3,
+        'Osong': 4,
+        'Daejeon': 5,
+        'Gimcheon_Gumi': 6,
+        'Dongdaegu': 7,
+        'Singyeongju': 8,
+        'Ulsan': 9,
+        'Busan': 10
+    }
+
 
 def get(date):
     return_dict = {}
@@ -16,9 +29,7 @@ def get(date):
     start = args['start']
     end = args['end']
 
-    print(date, train_num, start, end)
-
-    sql = f'SELECT * FROM K{date} WHERE train_num = "{train_num}"'
+    sql = f'SELECT * FROM K{date} WHERE train_num = {train_num}'
 
     try:
         cursor.execute(sql)
@@ -28,4 +39,17 @@ def get(date):
             return_dict[str(i + 1)] = '0'
         return return_dict
 
-    return '이제 시작이다.'
+    seat_data = list(cursor.fetchall())
+
+    for i in range(40):
+        count = 0
+        specific_data = list(seat_data[i])
+        for j in range(train_to_num[start], train_to_num[end]):
+            if specific_data[j+1] == 1:
+                return_dict[str(i+1)] = '1'
+                count += 1
+                break
+        if count == 0:
+            return_dict[str(i+1)] = '0'
+
+    return return_dict
